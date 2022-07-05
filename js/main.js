783288a66d7647902dd5f691b6edcab099e705f5
Subhadeep <myspotsub@gmail.com>
2022-07-05 21:41:59 +0530

    Explore section added

--- js/main.js
+++ js/main.js
@@ -87,9 +87,3 @@ const sr = ScrollReveal({
     delay: 100,
     // reset: true
 })
-
-sr.reveal(`.home__data`)
-sr.reveal(`.home__img`, {delay: 500})
-sr.reveal(`.about__img, .contact__box`,{origin: 'left'})
-sr.reveal(`.contact__form`,{origin: 'right'})
-sr.reveal(`.steps__card, .product__card, .questions__group, .footer`,{interval: 100})
